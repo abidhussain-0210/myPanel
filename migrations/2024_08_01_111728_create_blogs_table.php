ad34fd08eb500a6bb2047ddc45dd48105a634a7f
Abid Hussain Ansari <danishhussain0210@gmail.com>
2024-08-01 21:05:05 +0500

    Add files via upload

---
+++ migrations/2024_08_01_111728_create_blogs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('blog_name' , 100);
+            $table->integer('category_id');
+            $table->integer('tag_id');
+            $table->text('description');
+            $table->string('image' , 100);
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
